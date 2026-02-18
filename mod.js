930277ab5c7cc268e88424c79cc1b6a696fea41d
modydeo <73525480+modydeo@users.noreply.github.com>
2026-02-18 14:35:09 +0400

    Create mod.js

---
+++ mod.js
@@ -0,0 +1,21 @@
+// To create a mod:
+// Create a new Javascript file like this one.
+// Add the file to the mods folder on GitHub, or host it somewhere else.
+// https://github.com/R74nCom/sandboxels/tree/main/mods
+
+// To learn about modding, check the wiki: https://sandboxels.wiki.gg/wiki/Modding
+// Or join our Discord: https://r74n.com/discord/
+
+// To add it in the Mod Manager:
+// If it is in the mods folder, you can just use the name of the file. (example_mod.js)
+// If it is hosted somewhere else, you can use the full URL, including the HTTPS://.
+
+// Adding elements:
+elements.gubs = {
+    color: "#fffaf6",
+    behavior: behaviors.LIQUID,
+    category: "liquids",
+    state: "liquid",
+    density: 1100,
+    viscosity: 60000,
+}
