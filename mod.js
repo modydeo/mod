54b7002003b59bde5391670a1f07410059bffbb5
modydeo <73525480+modydeo@users.noreply.github.com>
2026-02-18 14:40:37 +0400

    Update mod.js

--- mod.js
+++ mod.js
@@ -1,8 +1,12 @@
 elements.gubs = {
-    color: ["#fffaf6", ],
-    behavior: behaviors.liquids,
-    category: "liquids",
-    state: "liquid",
-    stateHigh: "sp",
-    tempHigh: 1265
-}
+	color: "##fffaf6",
+	behavior: behaviors.LIQUID,
+	viscosity: 50000,
+	tempHigh: 260,
+	stateHigh: ["steam","steam","carbon_dioxide","methane"],
+	category:"liquids",
+	state: "liquid",
+	density: 1111.64,
+	stain: 0.05,
+	isFood: true
+};
